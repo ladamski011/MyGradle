35f4d4fa70f2b306688c05e2f84385fca65c449b
Lukasz <luad@o2.pl>
2021-03-07 17:17:25 +0100

    Initial commit

---
+++ src/main/java/org/example/firstprogram/FastUtilWrapper.java
@@ -0,0 +1,7 @@
+package org.example.firstprogram;
+
+import it.unimi.dsi.fastutil.doubles.Double2DoubleArrayMap;
+
+public class FastUtilWrapper {
+    private Double2DoubleArrayMap map = new Double2DoubleArrayMap();
+}
